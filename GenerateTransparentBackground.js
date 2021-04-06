1feb3a3bb317a08293be3287fefcb0e1bcbf194e
Linda Zheng <lindazheng1993@gmail.com>
2021-04-05 21:46:26 -0500

    word click widget script

---
+++ GenerateTransparentBackground.js
@@ -0,0 +1,250 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: magic;
+
+// This widget was created by Max Zeryck @mzeryck
+
+/*
+You can't add commit messages to gists, so I just want to say thanks to everyone who has used, modified, 
+and enjoyed this script. This version adds support for the iPhone 12 and 12 Pro, thanks to tslagle13!
+*/
+
+// Widgets are unique based on the name of the script.
+const filename = Script.name() + ".jpg"
+const files = FileManager.local()
+const path = files.joinPath(files.documentsDirectory(), filename)
+
+if (config.runsInWidget) {
+  let widget = new ListWidget()
+  widget.backgroundImage = files.readImage(path)
+  
+  // You can your own code here to add additional items to the "invisible" background of the widget.
+  
+  Script.setWidget(widget)
+  Script.complete()
+
+/*
+ * The code below this comment is used to set up the invisible widget.
+ * ===================================================================
+ */
+} else {
+  
+  // Determine if user has taken the screenshot.
+  var message
+  message = "Before you start, go to your home screen and enter wiggle mode. Scroll to the empty page on the far right and take a screenshot."
+  let exitOptions = ["Continue","Exit to Take Screenshot"]
+  let shouldExit = await generateAlert(message,exitOptions)
+  if (shouldExit) return
+  
+  // Get screenshot and determine phone size.
+  let img = await Photos.fromLibrary()
+  let height = img.size.height
+  let phone = phoneSizes()[height]
+  if (!phone) {
+    message = "It looks like you selected an image that isn't an iPhone screenshot, or your iPhone is not supported. Try again with a different image."
+    await generateAlert(message,["OK"])
+    return
+  }
+  
+  // Prompt for widget size and position.
+  message = "What size of widget are you creating?"
+  let sizes = ["Small","Medium","Large"]
+  let size = await generateAlert(message,sizes)
+  let widgetSize = sizes[size]
+  
+  message = "What position will it be in?"
+  message += (height == 1136 ? " (Note that your device only supports two rows of widgets, so the middle and bottom options are the same.)" : "")
+  
+  // Determine image crop based on phone size.
+  let crop = { w: "", h: "", x: "", y: "" }
+  if (widgetSize == "Small") {
+    crop.w = phone.small
+    crop.h = phone.small
+    let positions = ["Top left","Top right","Middle left","Middle right","Bottom left","Bottom right"]
+    let position = await generateAlert(message,positions)
+    
+    // Convert the two words into two keys for the phone size dictionary.
+    let keys = positions[position].toLowerCase().split(' ')
+    crop.y = phone[keys[0]]
+    crop.x = phone[keys[1]]
+    
+  } else if (widgetSize == "Medium") {
+    crop.w = phone.medium
+    crop.h = phone.small
+    
+    // Medium and large widgets have a fixed x-value.
+    crop.x = phone.left
+    let positions = ["Top","Middle","Bottom"]
+    let position = await generateAlert(message,positions)
+    let key = positions[position].toLowerCase()
+    crop.y = phone[key]
+    
+  } else if(widgetSize == "Large") {
+    crop.w = phone.medium
+    crop.h = phone.large
+    crop.x = phone.left
+    let positions = ["Top","Bottom"]
+    let position = await generateAlert(message,positions)
+    
+    // Large widgets at the bottom have the "middle" y-value.
+    crop.y = position ? phone.middle : phone.top
+  }
+  
+  // Crop image and finalize the widget.
+  let imgCrop = cropImage(img, new Rect(crop.x,crop.y,crop.w,crop.h))
+  
+  message = "Your widget background is ready. Would you like to use it as this script's background, or export the image for use in a different script or another widget app?"
+  const exportPhotoOptions = ["Use for this script","Export to Photos","Export to Files"]
+  const exportPhoto = await generateAlert(message,exportPhotoOptions)
+  
+  if (exportPhoto == 0) {
+    files.writeImage(path,imgCrop)
+  } else if (exportPhoto == 1) {
+    Photos.save(imgCrop)
+  } else if (exportPhoto == 2) {
+    await DocumentPicker.exportImage(imgCrop)
+  }
+  
+  Script.complete()
+}
+
+// Generate an alert with the provided array of options.
+async function generateAlert(message,options) {
+  
+  let alert = new Alert()
+  alert.message = message
+  
+  for (const option of options) {
+    alert.addAction(option)
+  }
+  
+  let response = await alert.presentAlert()
+  return response
+}
+
+// Crop an image into the specified rect.
+function cropImage(img,rect) {
+   
+  let draw = new DrawContext()
+  draw.size = new Size(rect.width, rect.height)
+  
+  draw.drawImageAtPoint(img,new Point(-rect.x, -rect.y))  
+  return draw.getImage()
+}
+
+// Pixel sizes and positions for widgets on all supported phones.
+function phoneSizes() {
+  let phones = {  
+  
+    // 12 and 12 Pro
+    "2532": {
+      small:  474,
+      medium: 1014,
+      large:  1062,
+      left:  78,
+      right: 618,
+      top:    231,
+      middle: 819,
+      bottom: 1407
+    },
+  
+    // 11 Pro Max, XS Max
+    "2688": {
+      small:  507,
+      medium: 1080,
+      large:  1137,
+      left:  81,
+      right: 654,
+      top:    228,
+      middle: 858,
+      bottom: 1488
+    },
+  
+    // 11, XR
+    "1792": {
+      small:  338,
+      medium: 720,
+      large:  758,
+      left:  54,
+      right: 436,
+      top:    160,
+      middle: 580,
+      bottom: 1000
+    },
+    
+    
+    // 11 Pro, XS, X
+    "2436": {
+      small:  465,
+      medium: 987,
+      large:  1035,
+      left:  69,
+      right: 591,
+      top:    213,
+      middle: 783,
+      bottom: 1353
+    },
+  
+    // Plus phones
+    "2208": {
+      small:  471,
+      medium: 1044,
+      large:  1071,
+      left:  99,
+      right: 672,
+      top:    114,
+      middle: 696,
+      bottom: 1278
+    },
+    
+    // SE2 and 6/6S/7/8
+    "1334": {
+      small:  296,
+      medium: 642,
+      large:  648,
+      left:  54,
+      right: 400,
+      top:    60,
+      middle: 412,
+      bottom: 764
+    },
+    
+    
+    // SE1
+    "1136": {
+      small:  282,
+      medium: 584,
+      large:  622,
+      left: 30,
+      right: 332,
+      top:  59,
+      middle: 399,
+      bottom: 399
+    },
+    
+    // 11 and XR in Display Zoom mode
+    "1624": {
+      small: 310,
+      medium: 658,
+      large: 690,
+      left: 46,
+      right: 394,
+      top: 142,
+      middle: 522,
+      bottom: 902 
+    },
+    
+    // Plus in Display Zoom mode
+    "2001" : {
+      small: 444,
+      medium: 963,
+      large: 972,
+      left: 81,
+      right: 600,
+      top: 90,
+      middle: 618,
+      bottom: 1146
+    },
+  }
+  return phones
+}
---
+++ WordClockWidget.js
@@ -0,0 +1,316 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: brown; icon-glyph: magic;
+
+/*=========================================================
+ * WIDGET CONFIGURATIONS
+ ========================================================*/
+
+const WIDGET_CONFIGURATIONS = {
+
+  // If this is true, it opens a prompt to select a 
+  // background image from the photo gallery,
+  // when the widget is run from the Scriptable app
+  useBackgroundImage: true,
+
+  // If useBackgroundImage is set to false, this background
+  // color gradient will be use. Feel free to set to your
+  // own colors
+  backgroundColor: [
+    new Color("#29323c"), 
+    new Color("#1c1c1c")
+  ],
+
+  // The font to use for the words in the word clock
+  font: 'Menlo-Bold',
+
+  // The font size to use for the words in the word clock
+  fontSize: 14,
+
+  // The color to use for the "highlighted" words.
+  // The "highlighted" words tells the time
+  textColorHighlighted: Color.white(),
+
+  // The color of the highlighted text shadow
+  textShadowHighlighted: Color.cyan(),
+
+  // The radius of the highlighted text shadow
+  textShadowHighlightedRadius: 3,
+
+  // The color of the words that are NOT highlighted
+  textColorBackground: Color.lightGray(),
+
+  // The opacity of the non-highlighted words 
+  // 1 -> opaque; 0 -> transparent
+  textAlphaBackground: 0.5,
+
+  // The spacing between the lines of words of the clock
+  spacingBetweenLines: 4,
+  
+  // The spacing between each individual word, within the
+  // same line
+  spacingBetweenWords: 8,
+}
+
+/*=========================================================
+ * WIDGET SET UP / PRESENTATION
+ ========================================================*/
+
+/**
+ * The words to display for the clock. If you want to use
+ * other words, make sure to update getHighlightedWords()
+ * accordingly.
+ */ 
+const TIME_WORDS_MATRIX = [
+  ['IT', 'IS', 'HALF', 'TEN'],
+  ['QUARTER', 'TWENTY'],
+  ['FIVE', 'MINUTES', 'TO'],
+  ['PAST', 'TWO', 'THREE'],
+  ['ONE', 'FOUR', 'FIVE'],
+  ['SIX', 'SEVEN', 'EIGHT'],
+  ['NINE', 'TEN', 'ELEVEN'],
+  ['TWELVE', 'O\'CLOCK'],
+];
+
+const widget = new ListWidget();
+
+await setBackground(widget, WIDGET_CONFIGURATIONS);
+
+drawWidget(widget, WIDGET_CONFIGURATIONS);
+
+if (config.runsInWidget) {
+  Script.setWidget(widget);
+  Script.complete();
+} else {
+  Script.setWidget(widget);
+  widget.presentSmall();
+  Script.complete();
+}
+
+/*=========================================================
+ * FUNCTIONS
+ ========================================================*/
+
+/**
+ * Main widget rendering function.
+ * 
+ * Lays out the widget as a vertical list of stacks, 
+ * containing texts.
+ */ 
+function drawWidget(widget, { 
+  font,
+  fontSize,
+  textColorHighlighted,
+  textShadowHighlighted,
+  textShadowHighlightedRadius,
+  textColorBackground,
+  textAlphaBackground,
+  spacingBetweenLines,
+  spacingBetweenWords,
+}) {
+  const mainStack = widget.addStack();
+  mainStack.layoutVertically();
+  mainStack.spacing = spacingBetweenLines;
+
+  const highlightedWords = getHighlightedWords();
+
+  for (let i = 0; i < TIME_WORDS_MATRIX.length; i++) {
+    const numWords = TIME_WORDS_MATRIX[i].length;
+    
+    const textStack = mainStack.addStack();
+    textStack.layoutHorizontally();
+    textStack.spacing = spacingBetweenWords;
+
+    for (let j = 0; j < numWords; j++) {
+      const text = textStack.addText(TIME_WORDS_MATRIX[i][j]);
+      text.font = new Font(font, fontSize);
+      if (highlightedWords[i] && highlightedWords[i][j]) {
+        text.textColor = textColorHighlighted;
+        text.shadowColor = textShadowHighlighted;
+        text.shadowRadius = textShadowHighlightedRadius;
+      } else {
+        text.textColor = textColorBackground;
+        text.alpha = textAlphaBackground;
+      }
+    }
+  }
+}
+
+/**
+ * Based on the input time, returns an array of arrays of
+ * which "time" words should be highlighted.
+ * 
+ * This is used as a look-up when adding texts to the 
+ * stack.
+ */
+function getHighlightedWords() {
+  // The dimensions of this should match the dimensions of
+  // TIME_WORDS_MATRIX
+  const defaultHighlights = [
+    [true, true, false, false],
+    [false, false],
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
+    [false, true],
+  ];
+
+  const date = new Date();
+  let hour = date.getHours();
+  const minute = date.getMinutes();
+
+  if (minute <= 30) {
+    // highlight "past"
+    defaultHighlights[3][0] = true;
+  } else {
+    // highlight "to"
+    defaultHighlights[2][2] = true;
+    // increment hour
+    hour = hour + 1;
+  }
+
+ if (minute >= 5 && minute < 10) {
+    // highlight "five"
+    defaultHighlights[2][0] = true;
+    // highlight "minutes"
+    defaultHighlights[2][1] = true;
+  } else if (minute >= 10 && minute < 15) {
+    // highlight "ten"
+    defaultHighlights[0][3] = true;
+    // highlight "minutes"
+    defaultHighlights[2][1] = true;
+  } else if (minute >= 15 && minute < 20) {
+    // highlight "quarter"
+    defaultHighlights[1][0] = true;
+  } else if (minute >= 20 && minute < 25) {
+    // highlight "twenty"
+    defaultHighlights[1][1] = true;
+    // highlight "minutes"
+    defaultHighlights[2][1] = true;
+  } else if (minute >= 25 && minute < 30) {
+    // highlight "twenty"
+    defaultHighlights[1][1] = true;
+    // highlight "five"
+    defaultHighlights[2][0] = true;
+    // highlight "minutes"
+    defaultHighlights[2][1] = true;
+  } else if (minute >= 30 && minute < 35) {
+    // highlight "half"
+    defaultHighlights[0][2] = true;
+  } else if (minute >= 35 && minute < 40) {
+    // highlight "twenty"
+    defaultHighlights[1][1] = true;
+    // highlight "five"
+    defaultHighlights[2][0] = true;
+    // highlight "minutes"
+    defaultHighlights[2][1] = true;
+  } else if (minute >= 40 && minute < 45) {
+    // highlight "twenty"
+    defaultHighlights[1][1] = true;
+    // highlight "minutes"
+    defaultHighlights[2][1] = true;
+  } else if (minute >= 45 && minute < 50) {
+    // highlight "quarter"
+    defaultHighlights[1][0] = true;
+  } else if (minute >= 50 && minute < 55) {
+    // highlight "ten"
+    defaultHighlights[0][3] = true;
+    // highlight "minutes"
+    defaultHighlights[2][1] = true;
+  } else if (minute >= 55) {
+    // highlight "five"
+    defaultHighlights[2][0] = true;
+    // highlight "minutes"
+    defaultHighlights[2][1] = true;
+  }
+  
+  if (hour > 12) {
+    hour = hour - 12;
+  }
+
+  if (hour === 1) {
+    defaultHighlights[4][0] = true;
+  } else if (hour === 2) {
+    defaultHighlights[3][1] = true;
+  } else if (hour === 3) {
+    defaultHighlights[3][2] = true;
+  } else if (hour === 4) {
+    defaultHighlights[4][1] = true;
+  } else if (hour === 5) {
+    defaultHighlights[4][2] = true;
+  } else if (hour === 6) {
+    defaultHighlights[5][0] = true;
+  } else if (hour === 7) {
+    defaultHighlights[5][1] = true;
+  } else if (hour === 8) {
+    defaultHighlights[5][2] = true;
+  } else if (hour === 9) {
+    defaultHighlights[6][0] = true;
+  } else if (hour === 10) {
+    defaultHighlights[6][1] = true;
+  } else if (hour === 11) {
+    defaultHighlights[6][2] = true;
+  } else if (hour === 12) {
+    defaultHighlights[7][0] = true;
+  } 
+
+  return defaultHighlights;
+}
+
+/**
+ * Sets the background on the widget.
+ * 
+ * If the script/widget is running as a widget, 
+ * set the background from the cache. 
+ * 
+ * If no background image is in the cache,
+ * default to the gray gradient.
+ * 
+ * If running in the app, prompt the user to select a 
+ * background image. Persist in cache.
+ */ 
+ async function setBackground(widget, { 
+   useBackgroundImage, 
+   backgroundColor,
+}) {
+  if (useBackgroundImage) {
+    // Determine if our image exists and when it was saved.
+    const files = FileManager.local();
+    const path = files.joinPath(
+      files.documentsDirectory(), 
+      'word-clock-widget-background'
+    );
+    const exists = files.fileExists(path);
+
+    // If it exists and we're running in the widget, 
+    // use photo from cache
+    // Or we're invoking the script to run FROM the widget 
+    // with a widgetParameter
+    if (exists && config.runsInWidget 
+        || args.widgetParameter === 'callback') {
+      widget.backgroundImage = files.readImage(path);
+
+      // If it's missing when running in the widget, 
+      // fallback to backgroundColor
+    } else if (!exists && config.runsInWidget) {
+      const bgColor = new LinearGradient();
+      bgColor.colors = backgroundColor;
+      bgColor.locations = [0.0, 1.0];
+      widget.backgroundGradient = bgColor;
+
+      // But if we're running in app, prompt the user for 
+      // the image.
+    } else if (config.runsInApp) {
+      const img = await Photos.fromLibrary();
+      widget.backgroundImage = img;
+      files.writeImage(path, img);
+    }
+  } else {
+    const bgColor = new LinearGradient();
+    bgColor.colors = backgroundColor;
+    bgColor.locations = [0.0, 1.0];
+    widget.backgroundGradient = bgColor;
+  }
+}
